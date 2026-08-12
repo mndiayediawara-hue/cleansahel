764e37d52b20ce54b15e7c75644aff1d26d611c4
mndiayediawara-hue <mndiayediawara@gmail.com>
2026-08-12 18:07:42 +0200

    FIX: RawMaterialLots.tsx

--- src/pages/RawMaterialLots.tsx
+++ src/pages/RawMaterialLots.tsx
@@ -1,3 +1,4 @@
+
 import { useState, useEffect, useRef } from 'react'
 import { useData } from '@/contexts/DataContext'
 import { useAuth } from '@/contexts/AuthContext'
@@ -450,5 +451,3 @@ function LotDetailModal({ lot, onClose, rawMaterials, suppliers, users }: any) {
     </Modal>
   )
 }
-
-
